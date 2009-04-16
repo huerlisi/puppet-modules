da553a9c681e2edf36b1ce01a616aea6b8d3e80b
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-04-16 10:58:04 +0000

    New module apache.

---
+++ apache/manifests/init.pp
@@ -0,0 +1,9 @@
+# Apache
+# ======
+class apache::server {
+        package {"apache2": ensure => installed }
+        service {"apache2":
+                ensure  => running,
+                require => Package["apache2"]
+        }
+}

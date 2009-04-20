402a5d75ccec72e27640e886794c6a15ec0222da
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-04-20 09:47:30 +0000

    Add class mysql::phpmyadmin.

--- mysql/manifests/init.pp
+++ mysql/manifests/init.pp
@@ -7,3 +7,20 @@ class mysql::server {
 class mysql::client {
         package {"mysql-client": ensure => installed }
 }
+
+# Web admin
+# =========
+class mysql::phpmyadmin {
+	package {"phpmyadmin": ensure => installed }
+	include mysql::phpmyadmin::apache2
+}
+
+class mysql::phpmyadmin::apache2 {
+	include apache::server
+
+	file {"/etc/apache2/conf.d/phpmyadmin.conf":
+		ensure  => "/etc/phpmyadmin/apache.conf",
+		require => Package["phpmyadmin"],
+		notify  => Service["apache2"]
+	}
+}

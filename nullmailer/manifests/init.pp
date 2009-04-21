43429ad8127dfbf0f2185c90704a22601c01322e
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-04-21 08:22:31 +0000

    New module nullmailer.

---
+++ nullmailer/manifests/init.pp
@@ -0,0 +1,14 @@
+# Nullmailer
+# ==========
+class nullmailer::client {
+        package {"nullmailer": ensure => installed }
+
+        file { "/etc/nullmailer/remotes":
+                content => template('nullmailer/etc/nullmailer/remotes'),
+		require => Package["nullmailer"]
+        }
+        file { "/etc/nullmailer/adminaddr":
+                content => template('nullmailer/etc/nullmailer/adminaddr'),
+		require => Package["nullmailer"]
+        }
+}

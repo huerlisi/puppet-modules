d754f472dd1eb749a01a2fb940727127894e8640
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-04-23 08:17:23 +0000

    Configure sesam ctrl authentication.

--- sep_sesam/manifests/init.pp
+++ sep_sesam/manifests/init.pp
@@ -3,6 +3,15 @@
 
 # TODO: add sesam repository
 class sep_sesam::client {
-	# TODO: unauthenticated...
+	# TODO: unauthenticated package error...
 	package {"sesam-cli": ensure => installed}
+
+	# TODO: Add Service declaration, not now
+	# as 'status' just starts in /etc/init.d/sesam
+	file {"/var/opt/sesam/var/ini/sm_ctrld.auth":
+		ensure  => present,
+		content => template("sep_sesam/var/opt/sesam/var/ini/sm_ctrld.auth"),
+		require => Package["sesam-cli"]
+		# TODO: notify Service["sesam"]
+	}
 }

d21e7ef064f78d81170e8d5b651dd99f0efd3a48
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-04-29 09:00:12 +0000

    Add repository for sep.

--- sep_sesam/manifests/init.pp
+++ sep_sesam/manifests/init.pp
@@ -1,10 +1,18 @@
 # SEP Sesam
 # =========
 
-# TODO: add sesam repository
 class sep_sesam::client {
+	# TODO: apt-get update
+	file {"/etc/apt/sources.list.d/lenny-sep-sesam.list":
+		ensure  => present,
+		content => template("sep_sesam/etc/apt/sources.list.d/lenny-sep-sesam.list")
+	}
+
 	# TODO: unauthenticated package error...
-	package {"sesam-cli": ensure => installed}
+	package {"sesam-cli":
+		ensure  => installed,
+		require => File["/etc/apt/sources.list.d/lenny-sep-sesam.list"]
+	}
 
 	# TODO: Add Service declaration, not now
 	# as 'status' just starts in /etc/init.d/sesam

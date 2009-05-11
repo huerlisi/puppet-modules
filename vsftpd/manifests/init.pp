e065ccf3763ae986692db206a99dd0eeec581afd
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-05-11 08:02:53 +0000

    Disable pam_shells.po for vsftpd to support /bin/false.

--- vsftpd/manifests/init.pp
+++ vsftpd/manifests/init.pp
@@ -6,4 +6,9 @@ class vsftpd::server {
 		ensure  => running,
 		require => Package["vsftpd"]
 	}
+
+	# See Debian Bug 419856: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=419856
+	file {"/etc/pam.d/vsftpd":
+		content => template("vsftpd/etc/pam.d/vsftpd")
+	}
 }

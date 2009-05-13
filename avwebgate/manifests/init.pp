30bd113560eb266fc785c58ae64943464801c4b3
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-05-13 14:01:11 +0000

    Implement avwebgate/squid integration; allow localnet access to squid.

--- avwebgate/manifests/init.pp
+++ avwebgate/manifests/init.pp
@@ -15,4 +15,23 @@ class avwebgate::server {
 		content => template("avwebgate/etc/avwebgate-scanner.conf"),
 		notify  => Service["avwebgate"]
 	}
+
+	file {"/var/log/antivir":
+		ensure => directory,
+		owner  => "root",
+		group  => "antivir",
+		mode   => "2775"
+	}
+}
+
+class avwebgate::squid {
+	include squid::server
+
+	file {"/etc/squid/conf.d/avwebgate.conf":
+		content => template("avwebgate/etc/squid/conf.d/avwebgate.conf"),
+		require => Service["avwebgate"],
+		require => File["/etc/squid/conf.d"],
+		notify  => Service["squid"]
+	}
+	
 }
--- squid/manifests/init.pp
+++ squid/manifests/init.pp
@@ -8,9 +8,29 @@ class squid::server {
 		require => Package["squid"]
 	}
 
+	file {"/etc/squid/conf.d":
+		ensure  => directory,
+		require => Package["squid"]
+	}
+
+	# Squid won't start without at least one .conf file in conf.d ...
+	file {"/etc/squid/conf.d/dummy.conf":
+		ensure  => present,
+		require => File["/etc/squid/conf.d"]
+	}
+
+	file {"/etc/squid/conf.d/localnet.acl":
+		ensure  => present,
+		content => template("squid/etc/squid/conf.d/localnet.acl"),
+		require => File["/etc/squid/conf.d"],
+		require => Package["squid"],
+		notify  => Service["squid"],
+	}
+
 	file {"/etc/squid/squid.conf":
 		content => template("squid/etc/squid/squid.conf"),
 		notify  => Service["squid"],
-		require => Package["squid"]
+		require => Package["squid"],
+		require => File["/etc/squid/conf.d"]
 	}
 }

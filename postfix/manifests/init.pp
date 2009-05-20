e637219f009f9c5095a61a613cb3c1a875be8f97
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-05-20 13:09:41 +0000

    Add master.cf template for postfix.

--- postfix/manifests/init.pp
+++ postfix/manifests/init.pp
@@ -11,6 +11,13 @@ class postfix::server {
 	file {"/etc/postfix/main.cf":
 		ensure  => present,
 		content => template("postfix/etc/postfix/main.cf"),
+		require => Package["postfix"],
+		notify  => Service["postfix"]
+	}
+	file {"/etc/postfix/master.cf":
+		ensure  => present,
+		content => template("postfix/etc/postfix/master.cf"),
+		require => Package["postfix"],,
 		notify  => Service["postfix"]
 	}
 }

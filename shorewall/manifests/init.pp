b1119af53eb19e68c27fa89d6a1320fa1751308d
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-05-25 09:05:57 +0000

    Add shorewall.conf as packaged.

--- shorewall/manifests/init.pp
+++ shorewall/manifests/init.pp
@@ -2,4 +2,9 @@
 # ========
 class shorewall::daemon {
 	package { "shorewall-perl": ensure => installed }
+
+	file { "/etc/shorewall/shorewall.conf":
+		ensure  => present,
+		content => template("shorewall/etc/shorewall/shorewall.conf")
+	}
 }

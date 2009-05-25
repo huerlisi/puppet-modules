97af31408fd727586c88072460f7424c90b1795b
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-05-25 09:06:42 +0000

    Add shorewall config files from two-interface example.

--- shorewall/manifests/init.pp
+++ shorewall/manifests/init.pp
@@ -7,4 +7,29 @@ class shorewall::daemon {
 		ensure  => present,
 		content => template("shorewall/etc/shorewall/shorewall.conf")
 	}
+
+	file { "/etc/shorewall/interfaces":
+		ensure  => present,
+		content => template("shorewall/etc/shorewall/interfaces")
+	}
+	file { "/etc/shorewall/rules":
+		ensure  => present,
+		content => template("shorewall/etc/shorewall/rules")
+	}
+	file { "/etc/shorewall/masq":
+		ensure  => present,
+		content => template("shorewall/etc/shorewall/masq")
+	}
+	file { "/etc/shorewall/zones":
+		ensure  => present,
+		content => template("shorewall/etc/shorewall/zones")
+	}
+	file { "/etc/shorewall/policy":
+		ensure  => present,
+		content => template("shorewall/etc/shorewall/policy")
+	}
+	file { "/etc/shorewall/routestopped":
+		ensure  => present,
+		content => template("shorewall/etc/shorewall/routestopped")
+	}
 }

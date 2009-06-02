c9ba30b8038e07dbff64e595056c29d5bc8bf9d9
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-06-02 10:00:20 +0000

    New class apt::client setting proxy.

--- apt/manifests/init.pp
+++ apt/manifests/init.pp
@@ -1,5 +1,11 @@
 # APT
 # ===
+class apt::client {
+	file {"/etc/apt/apt.conf.d/proxy":
+		content => template("apt/etc/apt/apt.conf.d/proxy")
+	}
+}
+
 class apt::unattended-upgrades {
 	package {"unattended-upgrades": ensure => installed}
 }

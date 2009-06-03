340d14b57ff11fc0deb3eb1cb04b05dcf76ddd7a
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-06-03 08:28:41 +0000

    Use template for /etc/sudoers file.

--- sudo/manifests/init.pp
+++ sudo/manifests/init.pp
@@ -3,9 +3,9 @@
 class sudo::client {
         package {"sudo": ensure => installed }
 
-        exec { "Allow ALL to group %ita":
-                command => "/bin/echo '%ita ALL=(ALL) ALL' >>/etc/sudoers",
-                unless  => "/bin/grep '%ita ALL=(ALL) ALL' /etc/sudoers",
-                require => Package["sudo"]
-        }
+	file { "/etc/sudoers":
+		content => template("sudo/etc/sudoers"),
+		mode    => "0440",
+		require => Package["sudo"]
+	}
 }

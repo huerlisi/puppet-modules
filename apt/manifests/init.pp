c8e97332c2b99e4831bc09fca8c217a75790497e
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-06-16 12:34:10 +0000

    Disable installation of recommends on apt-get.

--- apt/manifests/init.pp
+++ apt/manifests/init.pp
@@ -4,6 +4,9 @@ class apt::client {
 	file {"/etc/apt/apt.conf.d/proxy":
 		content => template("apt/etc/apt/apt.conf.d/proxy")
 	}
+	file {"/etc/apt/apt.conf.d/no-install-recommends":
+		content => template("apt/etc/apt/apt.conf.d/no-install-recommends")
+	}
 }
 
 class apt::unattended-upgrades {

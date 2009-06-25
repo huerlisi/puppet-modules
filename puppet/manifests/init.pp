be2aff1015f56ee1cf7965125f2f0361ee746328
Simon Hürlimann (ZytoLabor) <simon.huerlimann@cyt.ch>
2009-06-25 14:14:20 +0200

    Add dependency on puppetmaster package for puppetmaster service; some indentation fixes in same module.

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,25 +1,27 @@
 # Puppet
 # ======
 class puppet::server {
-    package { "puppetmaster": ensure => installed }
-    service { "puppetmaster": ensure => running }
+	package { "puppetmaster": ensure => installed }
+	service { "puppetmaster":
+		ensure  => running,
+		require => Package["puppetmaster"]
+	}
 
-#    file { "/etc/puppet/fileserver.conf":
-#            ensure  => file,
-#            content => template('puppet/etc/puppet/fileserver.conf')
-#    }
+#	file { "/etc/puppet/fileserver.conf":
+#		ensure  => file,
+#		content => template('puppet/etc/puppet/fileserver.conf')
+#	}
 
-    file {
-        ["/etc/puppet/manifests", "/etc/puppet/manifests/nodes", "/etc/puppet/modules"]:
-            ensure => directory,
-            mode   => "2775",
-            owner  => "root",
-#            group  => "cyt-sysadmins"
-    }
+	file { ["/etc/puppet/manifests", "/etc/puppet/manifests/nodes", "/etc/puppet/modules"]:
+		ensure => directory,
+		mode   => "2775",
+		owner  => "root",
+#		group  => "cyt-sysadmins"
+	}
 
-    file { "/etc/puppet/modules/*":
-        mode => "2775",
-        owner => "root",
-#        group => "cyt-sysadmins"
-    }
+	file { "/etc/puppet/modules/*":
+		mode => "2775",
+		owner => "root",
+#		group => "cyt-sysadmins"
+	}
 }

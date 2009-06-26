ec44a488119a9d413ea942c5d808b18058f0eef8
Simon Hürlimann (ZytoLabor) <simon.huerlimann@cyt.ch>
2009-06-26 11:43:57 +0000

    No timeout for building ltsp client image.

--- ltsp/manifests/init.pp
+++ ltsp/manifests/init.pp
@@ -6,5 +6,6 @@ class ltsp::server {
 	exec {"ltsp-build-client --squashfs-image --late-packages xserver-xorg-video-geode":
 		creates => "/opt/ltsp/i386",
 		path    => ["/usr/local/sbin", "/usr/local/bin", "/usr/sbin", "/usr/bin", "/sbin", "/bin"],
+		timeout => '-1'
 	}
 }

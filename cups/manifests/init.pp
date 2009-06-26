d80f271c41919f10b5627a03e14b2b5ed8d4e792
Simon Hürlimann (ZytoLabor) <simon.huerlimann@cyt.ch>
2009-06-26 13:19:48 +0000

    Add default cupsd.conf as template for cups; add cups service.

--- cups/manifests/init.pp
+++ cups/manifests/init.pp
@@ -2,4 +2,16 @@
 # ====
 class cups::server {
 	package {"cups": ensure => installed}
+
+	service {"cups":
+		ensure  => running,
+		require => Package["cups"]
+	}
+
+	file {"/etc/cups/cupsd.conf":
+		ensure => file,
+		content => template("cups/etc/cups/cupsd.conf"),
+		require => Package["cups"],
+		notify  => Service["cups"]
+	}
 }

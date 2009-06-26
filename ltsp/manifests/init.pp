91b3b81640d196b311d1b8f24d0078ff24e72924
Simon Hürlimann (ZytoLabor) <simon.huerlimann@cyt.ch>
2009-06-26 11:36:09 +0000

    Build ltsp client image when /opt/ltsp/i386 not there, yet

--- ltsp/manifests/init.pp
+++ ltsp/manifests/init.pp
@@ -2,4 +2,9 @@
 # ====
 class ltsp::server {
 	package {"ltsp-server-standalone": ensure => installed }
+
+	exec {"ltsp-build-client --squashfs-image --late-packages xserver-xorg-video-geode":
+		creates => "/opt/ltsp/i386",
+		path    => ["/usr/local/sbin", "/usr/local/bin", "/usr/sbin", "/usr/bin", "/sbin", "/bin"],
+	}
 }

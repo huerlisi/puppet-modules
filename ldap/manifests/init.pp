4f0a0bd2073fe0b817046286e6cabac7108db008
Simon Hürlimann (ZytoLabor) <simon.huerlimann@cyt.ch>
2009-07-02 13:34:49 +0200

    Add some configuration files for ldap-utils and slapd.

--- ldap/manifests/init.pp
+++ ldap/manifests/init.pp
@@ -2,6 +2,12 @@
 # ====
 class ldap::utils {
 	package {"ldap-utils": ensure => installed}
+
+	file {"/etc/ldap/ldap.conf":
+		ensure  => file,
+		content => template("ldap/etc/ldap/ldap.conf"),
+		require => Package["ldap-utils"]
+	}
 }
 
 class ldap::server {
@@ -10,6 +16,16 @@ class ldap::server {
 	service {"slapd":
 		ensure  => running,
 		require => Package["slapd"]
+	}
+
+	file {"/etc/ldap/slapd.conf":
+		ensure  => file,
+		owner   => 'root',
+		group   => 'openldap',
+		mode    => '640',
+		content => template("ldap/etc/ldap/slapd.conf"),
+		require => Package["slapd"],
+		notify  => Service["slapd"]
 	}
 
 	include ldap::utils

101bd47ba34f6f37384a6038e0f85a63e5489334
Simon Hürlimann (ZytoLabor) <simon.huerlimann@cyt.ch>
2009-07-03 09:59:53 +0200

    Add support for ldap PAM and NIS by providing classes ldap::nis and ldap::pam.

--- ldap/manifests/init.pp
+++ ldap/manifests/init.pp
@@ -1,13 +1,17 @@
 # LDAP
 # ====
-class ldap::utils {
-	package {"ldap-utils": ensure => installed}
-
+class ldap::client {
+	file {"/etc/ldap": ensure => directory}
 	file {"/etc/ldap/ldap.conf":
 		ensure  => file,
 		content => template("ldap/etc/ldap/ldap.conf"),
-		require => Package["ldap-utils"]
+		require => File["/etc/ldap"]
 	}
+}
+
+class ldap::utils {
+	include ldap::client
+	package {"ldap-utils": ensure => installed}
 }
 
 class ldap::server {
@@ -31,3 +35,38 @@ class ldap::server {
 	include ldap::utils
 }
 
+class ldap::pam {
+	include ldap::client
+	include ldap::nis
+
+	package {"libpam-ldap": ensure => installed}
+
+	file {"/etc/pam_ldap.conf":
+		content => template("ldap/etc/pam_ldap.conf")
+	}
+
+	file { "/etc/pam.d/common-account":
+	        content => template("ldap/etc/pam.d/common-account")
+	}
+	file { "/etc/pam.d/common-auth":
+        	content => template("ldap/etc/pam.d/common-auth")
+	}
+	file { "/etc/pam.d/common-password":
+	        content => template("ldap/etc/pam.d/common-password")
+	}
+	file { "/etc/pam.d/common-session":
+	        content => template("ldap/etc/pam.d/common-session")
+	}
+}
+
+class ldap::nis {
+	package {"libnss-ldapd": ensure => installed}
+
+	file {"/etc/nss-ldapd.conf":
+		content => template("ldap/etc/nss-ldapd.conf")
+	}
+
+	file {"/etc/nsswitch.conf":
+		content => template("ldap/etc/nsswitch.conf")
+	}
+}

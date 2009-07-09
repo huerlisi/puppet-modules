bd6fa9007892d4d2b03be8e3a8d120eab8a2dc76
Simon Hürlimann (ZytoLabor) <simon.huerlimann@cyt.ch>
2009-07-09 08:36:41 +0200

    New module tftp providing tftp::server using tftpd-hpa.

---
+++ tftp/manifests/init.pp
@@ -0,0 +1,17 @@
+# TFTP
+# ====
+class tftp::server {
+        package { "tftpd-hpa": ensure => installed }
+
+        service { "tftpd-hpa":
+                ensure => running,
+                require => Package["tftpd-hpa"]
+        }
+
+	file { "/etc/default/tftpd-hpa":
+		ensure  => file,
+		content => template("tftp/etc/default/tftpd-hpa"),
+		require => Package["tftpd-hpa"],
+		notify  => Service["tftpd-hpa"]
+	}
+}

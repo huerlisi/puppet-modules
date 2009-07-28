916dd1e8ad31faab713baa423cfc419cfa45c4d4
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-07-28 12:16:00 +0200

    New module php including php::gd and php::imagick.

---
+++ php/manifests/init.pp
@@ -0,0 +1,21 @@
+# PHP
+# ===
+class php::base {
+	package { "php5": ensure => installed }
+}
+
+class php::apache2-mod {
+	package { "libapache2-mod-php5": ensure => installed }
+}
+
+# Modules
+# =======
+# Should probably implemented using virtual resources or something...
+
+class php::gd {
+	package {"php5-gd": ensure => installed}
+}
+
+class php::imagick {
+	package {"php5-imagick": ensure => installed}
+}

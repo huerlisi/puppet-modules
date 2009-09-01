22f626e86810809ab256e0c11fdf0c5f1cb0bbe6
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-09-01 14:07:25 +0200

    Support parameter $shorewall_module

--- shorewall/manifests/init.pp
+++ shorewall/manifests/init.pp
@@ -1,5 +1,37 @@
 # Firewall
 # ========
+class shorewall::config {
+	file { "/etc/shorewall/shorewall.conf":
+		ensure  => present,
+		content => template("$shorewall_module/etc/shorewall/shorewall.conf")
+	}
+
+	file { "/etc/shorewall/interfaces":
+		ensure  => present,
+		content => template("$shorewall_module/etc/shorewall/interfaces")
+	}
+	file { "/etc/shorewall/rules":
+		ensure  => present,
+		content => template("$shorewall_module/etc/shorewall/rules")
+	}
+	file { "/etc/shorewall/masq":
+		ensure  => present,
+		content => template("$shorewall_module/etc/shorewall/masq")
+	}
+	file { "/etc/shorewall/zones":
+		ensure  => present,
+		content => template("$shorewall_module/etc/shorewall/zones")
+	}
+	file { "/etc/shorewall/policy":
+		ensure  => present,
+		content => template("$shorewall_module/etc/shorewall/policy")
+	}
+	file { "/etc/shorewall/routestopped":
+		ensure  => present,
+		content => template("$shorewall_module/etc/shorewall/routestopped")
+	}
+}
+
 class shorewall::daemon {
 	package { "shorewall-perl": ensure => installed }
 
@@ -8,33 +40,11 @@ class shorewall::daemon {
 		content => template("shorewall/etc/default/shorewall")
 	}
 
-	file { "/etc/shorewall/shorewall.conf":
-		ensure  => present,
-		content => template("shorewall/etc/shorewall/shorewall.conf")
-	}
-
-	file { "/etc/shorewall/interfaces":
-		ensure  => present,
-		content => template("shorewall/etc/shorewall/interfaces")
-	}
-	file { "/etc/shorewall/rules":
-		ensure  => present,
-		content => template("shorewall/etc/shorewall/rules")
-	}
-	file { "/etc/shorewall/masq":
-		ensure  => present,
-		content => template("shorewall/etc/shorewall/masq")
-	}
-	file { "/etc/shorewall/zones":
-		ensure  => present,
-		content => template("shorewall/etc/shorewall/zones")
-	}
-	file { "/etc/shorewall/policy":
-		ensure  => present,
-		content => template("shorewall/etc/shorewall/policy")
-	}
-	file { "/etc/shorewall/routestopped":
-		ensure  => present,
-		content => template("shorewall/etc/shorewall/routestopped")
+	if $shorewall_module {
+		include shorewall::config
+	} else {
+		$shorewall_module = 'shorewall'
+		include shorewall::config
+	
 	}
 }

fe09ea671c2dec5a4d72cf972a191a3185b2c798
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-09-07 11:01:51 +0200

    Move primary interface declaration into network::interface::primary, out of network::interfaces

--- network/manifests/init.pp
+++ network/manifests/init.pp
@@ -11,48 +11,32 @@ define network::interface($iface_name, $iface_address = "", $iface_netmask = "",
 	}
 }
 
-
 class network::interfaces {
 	# Workaround for Debian Bug #159884
 	file { "/etc/network/interfaces.d/": ensure => directory }
 
-	if $network_interface_template {
-		network::interface {"system":
-			iface_name        => "eth0",
-			iface_address     => $host_address,
-			iface_gateway     => $network_gateway,
-			iface_nameservers => $network_nameservers,
-			iface_search      => $network_search,
-			iface_routes      => $network_routes,
-			iface_options     => $network_options,
-			iface_template    => $network_interface_template
-		}
-	} else {
-		file {"/etc/network/interfaces.d/system":
-			ensure => absent,
-			notify  => Exec["Generating /etc/network/interfaces"]
-		}
-		network::interface {"10-lo":
-			iface_name        => "lo",
-			iface_template    => "network/etc/network/interfaces.d/lo"
-		}
-		network::interface {"20-primary":
-			iface_name        => "eth0",
-			iface_address     => $host_address,
-			iface_gateway     => $network_gateway,
-			iface_netmask     => $network_netmask,
-			iface_nameservers => $network_nameservers,
-			iface_search      => $network_search,
-			iface_routes      => $network_routes,
-			iface_options     => $network_options,
-			iface_template    => "network/etc/network/interfaces.d/static"
-		}
+	network::interface {"10-lo":
+		iface_name        => "lo",
+		iface_template    => "network/etc/network/interfaces.d/lo"
 	}
-
 	exec { "Generating /etc/network/interfaces":
 		path        => ["/usr/local/sbin", "/usr/local/bin", "/usr/sbin", "/usr/bin", "/sbin", "/bin"],
                 command     => "cat $(run-parts --list /etc/network/interfaces.d) > /etc/network/interfaces",
 		refreshonly => true,
-		require => File["/etc/network/interfaces.d/"]
+		require     => File["/etc/network/interfaces.d/"]
+	}
+}
+
+class network::interface::primary {
+	network::interface {"20-primary":
+		iface_name        => "eth0",
+		iface_address     => $host_address,
+		iface_gateway     => $network_gateway,
+		iface_netmask     => $network_netmask,
+		iface_nameservers => $network_nameservers,
+		iface_search      => $network_search,
+		iface_routes      => $network_routes,
+		iface_options     => $network_options,
+		iface_template    => "network/etc/network/interfaces.d/static"
 	}
 }

91cec265f63fd2bb21bf448f8134a0c2e424cf76
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-09-07 13:25:04 +0200

    Add drbd.conf as provided by package maintainer

--- drbd/manifests/init.pp
+++ drbd/manifests/init.pp
@@ -3,4 +3,9 @@
 class drbd::daemon {
 	package { "drbd8-utils": ensure => installed }
 	package { "drbd8-modules-2.6-xen-$architecture": ensure => installed }
+
+	file {"/etc/drbd.conf":
+		ensure  => present,
+		content => template("drbd/etc/drbd.conf")
+	}
 }

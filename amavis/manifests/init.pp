41e041130d9623cf80700884b9521911048ed0f4
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-09-22 18:08:54 +0200

    Add requires to amavis

--- amavis/manifests/init.pp
+++ amavis/manifests/init.pp
@@ -12,7 +12,7 @@ class amavis::daemon {
 
 	file {"/etc/amavis/conf.d/50-user":
 		ensure  => present,
-		content => template("amavis//etc/amavis/conf.d/50-user"),
+		content => template("amavis/etc/amavis/conf.d/50-user"),
 		notify  => Service["amavis"]
 	}
 }
@@ -22,7 +22,8 @@ class amavis::daemon {
 class amavis::spam {
 	file {"/etc/amavis/conf.d/50-enable-spam-checks":
 		ensure  => present,
-		content => template("amavis//etc/amavis/conf.d/50-enable-spam-checks"),
+		content => template("amavis/etc/amavis/conf.d/50-enable-spam-checks"),
+		require => Package["amavisd-new"],
 		notify  => Service["amavis"]
 	}
 }
@@ -33,7 +34,8 @@ class amavis::virus {
 	# TODO: add group clamav to amavis
 	file {"/etc/amavis/conf.d/50-enable-virus-checks":
 		ensure  => present,
-		content => template("amavis//etc/amavis/conf.d/50-enable-virus-checks"),
+		content => template("amavis/etc/amavis/conf.d/50-enable-virus-checks"),
+		require => Package["amavisd-new"],
 		notify  => Service["amavis"]
 	}
 }

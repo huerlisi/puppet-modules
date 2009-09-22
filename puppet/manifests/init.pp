0c576c1244ed1a81aff9bff53eb74935375eca5e
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-09-22 18:01:47 +0200

    No file mode settings for /etc/puppet.

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -14,16 +14,4 @@ class puppet::server {
 #		content => template('puppet/etc/puppet/fileserver.conf')
 #	}
 
-	file { ["/etc/puppet/manifests", "/etc/puppet/manifests/nodes", "/etc/puppet/modules"]:
-		ensure => directory,
-		mode   => "2775",
-		owner  => "root",
-#		group  => "cyt-sysadmins"
-	}
-
-	file { "/etc/puppet/modules/*":
-		mode => "2775",
-		owner => "root",
-#		group => "cyt-sysadmins"
-	}
 }

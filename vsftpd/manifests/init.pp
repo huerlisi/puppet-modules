f7adb703d156d34be00758ba97c5d11822c1b0c3
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-11-11 10:47:44 +0100

    Add stock vsftpd.conf

--- vsftpd/manifests/init.pp
+++ vsftpd/manifests/init.pp
@@ -7,6 +7,12 @@ class vsftpd::server {
 		require => Package["vsftpd"]
 	}
 
+	file {"/etc/vsftpd.conf":
+		content => template("vsftpd/etc/vsftpd.conf"),
+		require => Package["vsftpd"],
+		notify  => Service["vsftpd"]
+	}
+
 	# See Debian Bug 419856: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=419856
 	file {"/etc/pam.d/vsftpd":
 		content => template("vsftpd/etc/pam.d/vsftpd")

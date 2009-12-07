17a1debd01d39b8ca9c8005ba25f2361c72a3dbf
Simon Hürlimann (SBS) <simon.huerlimann@cyt.ch>
2009-12-07 14:24:09 +0100

    New class rsyslog::server.

--- rsyslog/manifests/init.pp
+++ rsyslog/manifests/init.pp
@@ -1,14 +1,24 @@
 # Rsyslog
 # =======
-class rsyslog::client {
+class rsyslog::daemon {
         package {"rsyslog": ensure => installed }
         service {"rsyslog":
                 ensure  => running,
                 require => Package["rsyslog"]
         }
+}
 
+class rsyslog::client inherits rsyslog::daemon {
         file { "/etc/rsyslog.d/to-remote.conf":
                 content => template('rsyslog/etc/rsyslog.d/to-remote.conf'),
+                notify  => Service["rsyslog"],
+                require => Package["rsyslog"]
+        }
+}
+
+class rsyslog::server inherits rsyslog::daemon {
+        file { "/etc/rsyslog.d/allow-remote.conf":
+                content => template('rsyslog/etc/rsyslog.d/allow-remote.conf'),
                 notify  => Service["rsyslog"],
                 require => Package["rsyslog"]
         }

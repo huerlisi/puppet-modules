f1ef718270e64d4dcffdb52ac335c0254f425996
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-03-08 23:41:02 +0100

    On intrepid and karmic use old-school slapd.conf, not cn=config.

--- ldap/manifests/init.pp
+++ ldap/manifests/init.pp
@@ -45,6 +45,17 @@ class ldap::server {
 		notify  => Service["slapd"]
 	}
 
+	case $lsbdistcodename {
+		'intrepid', 'karmic': {
+                        file {"/etc/default/slapd":
+                                ensure  => file,
+                                content => template("ldap/etc/default/slapd"),
+                                require => Package["slapd"],
+                                notify  => Service["slapd"]
+                        }
+		}
+
+	}
 	include ldap::utils
 }
 

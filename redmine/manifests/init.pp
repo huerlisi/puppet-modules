cc7f1a54805a0b6ef7a11a9818ea1958333816ed
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-03-13 23:18:08 +0100

    Add librmagick-ruby as package to redmine::webapp.

--- redmine/manifests/init.pp
+++ redmine/manifests/init.pp
@@ -1,5 +1,5 @@
 # Redmine
 # =======
 class redmine::webapp {
-	package {["redmine", "redmine-mysql"]: ensure => installed }
+	package {["redmine", "redmine-mysql", "librmagick-ruby"]: ensure => installed }
 }

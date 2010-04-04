83cdb66b894205ef8f274b5c649ddf3cf9cdbb29
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-04-04 23:50:05 +0200

    Use relative paths for module enabling links.

--- apache2/manifests/init.pp
+++ apache2/manifests/init.pp
@@ -12,7 +12,7 @@ class apache2::server {
 # ==============
 define apache2_module() {
         file { "/etc/apache2/mods-enabled/$title":
-                ensure  => "/etc/apache2/mods-available/$title",
+                ensure  => "../mods-available/$title",
                 notify  => Service["apache2"],
                 require => Package["apache2"]
         }
@@ -20,13 +20,13 @@ define apache2_module() {
 
 class apache2::mod_ldap {
         file { "/etc/apache2/mods-enabled/authnz_ldap.load":
-                ensure  => "/etc/apache2/mods-available/authnz_ldap.load",
+                ensure  => "../mods-available/authnz_ldap.load",
                 notify  => Service["apache2"],
                 require => Package["apache2"]
         }
 
         file { "/etc/apache2/mods-enabled/ldap.load":
-                ensure  => "/etc/apache2/mods-available/ldap.load",
+                ensure  => "../mods-available/ldap.load",
                 notify  => Service["apache2"],
                 require => Package["apache2"]
         }
@@ -34,12 +34,12 @@ class apache2::mod_ldap {
 
 class apache2::mod_deflate {
         file { "/etc/apache2/mods-enabled/deflate.load":
-                ensure  => "/etc/apache2/mods-available/deflate.load",
+                ensure  => "../mods-available/deflate.load",
                 notify  => Service["apache2"],
                 require => Package["apache2"]
         }
         file { "/etc/apache2/mods-enabled/deflate.conf":
-                ensure  => "/etc/apache2/mods-available/deflate.conf",
+                ensure  => "../mods-available/deflate.conf",
                 notify  => Service["apache2"],
                 require => Package["apache2"]
         }
@@ -47,17 +47,17 @@ class apache2::mod_deflate {
 
 class apache2::mod_proxy {
         file { "/etc/apache2/mods-enabled/proxy.load":
-                ensure  => "/etc/apache2/mods-available/proxy.load",
+                ensure  => "../mods-available/proxy.load",
                 notify  => Service["apache2"],
                 require => Package["apache2"]
         }
         file { "/etc/apache2/mods-enabled/proxy.conf":
-                ensure  => "/etc/apache2/mods-available/proxy.conf",
+                ensure  => "../mods-available/proxy.conf",
                 notify  => Service["apache2"],
                 require => Package["apache2"]
         }
         file { "/etc/apache2/mods-enabled/proxy_http.load":
-                ensure  => "/etc/apache2/mods-available/proxy_http.load",
+                ensure  => "../mods-available/proxy_http.load",
                 notify  => Service["apache2"],
                 require => Package["apache2"]
         }

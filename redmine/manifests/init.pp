41d096056c349c26d429ab8be4068392fdaf5a56
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-04-11 15:37:10 +0200

    class redmine::webapp now inherits rails::webapp, includes mysql::server and uses dpkg-statoverride on environment.rb.

--- redmine/manifests/init.pp
+++ redmine/manifests/init.pp
@@ -1,5 +1,23 @@
 # Redmine
 # =======
-class redmine::webapp {
-	package {["redmine", "redmine-mysql"]: ensure => installed }
+class redmine::webapp inherits rails::webapp {
+	include mysql::server
+
+	package {"redmine-mysql":
+		ensure  => installed,
+		require => Package["mysql-server"]
+	}
+
+	package {"redmine":
+		ensure  => installed,
+		require => Package["redmine-mysql"],
+		notify  => Exec["Set www-data as owner of environment.rb"]
+	}
+
+	exec {"Set www-data as owner of environment.rb":
+		command => "sudo dpkg-statoverride --update --add www-data root 644 /usr/share/redmine/config/environment.rb",
+		path        => ["/usr/local/sbin", "/usr/local/bin", "/usr/sbin", "/usr/bin", "/sbin", "/bin"],
+		refreshonly => true,
+		notify      => Service["apache2"]
+	}
 }

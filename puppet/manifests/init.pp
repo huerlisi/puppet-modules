1d4ea6c296b23b6144ce4e5f9ea28a8d9973d73f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-05-06 13:37:11 +0200

    New class puppet::store.

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -13,5 +13,16 @@ class puppet::server {
 #		ensure  => file,
 #		content => template('puppet/etc/puppet/fileserver.conf')
 #	}
+}
 
+class puppet::store {
+	import "mysql"
+
+	@@mysql_database { "puppet": ensure => present, tag => 'infra01.dmz' }
+	@@mysql_user { "puppet@$fqdn":
+		ensure        => present,
+		password_hash => '*362C0A1E240F3149F49DF9B55A3D53E1CD0B7E49',
+		tag => 'infra01.dmz'
+	}
+	@@mysql_grant { "puppet@$fqdn/puppet": privileges => 'all', tag => 'infra01.dmz' }
 }

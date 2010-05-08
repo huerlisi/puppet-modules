ca388bcb62e4a37609ccd79f2a2704ddcbc93556
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-05-08 10:57:01 +0200

    New classes zarafa::server and zarafa::mysql; require canonical partner repository for packages.

--- zarafa/manifests/init.pp
+++ zarafa/manifests/init.pp
@@ -1,10 +1,51 @@
 # Zarafa
 # ======
-class zarafa::webapp inherits php::apache2 {
-	include mysql::server
+class zarafa::apt {
+	apt::deb-list {"canonical-lucid-partner":
+		repository => "http://archive.canonical.com/ubuntu",
+		components => ["partner"]
+	}
+}
 
-	package {["zarafa", "zarafa-webaccess"]:
+class zarafa::mysql {
+	@@mysql_database { "zarafa": ensure => present, tag => "mysql_$zone" }
+	@@mysql_user { "zarafa@$hostname.$network":
+		ensure        => present,
+		password_hash => mysql_password($mysql_password),
+		tag           => "mysql_$zone"
+	}
+	@@mysql_grant { "zarafa@$hostname.$network/zarafa": privileges => 'all', tag => "mysql_$zone" }
+}
+
+class zarafa::server {
+	include zarafa::apt
+	include zarafa::mysql
+
+	package {"zarafa":
 		ensure  => installed,
-		require => Apt::Ppa["huerlisi-ppa"]
+		require => Apt::Deb-list["canonical-lucid-partner"]
+	}
+
+	service {"zarafa-server":
+		ensure  => running,
+		require => Package["zarafa"]
+	}
+
+	file {"/etc/zarafa/server.cfg":
+		ensure => present,
+		mode => "0640",
+		owner => "root",
+		content => template("zarafa/etc/zarafa/server.cfg"),
+		require => Package["zarafa"],
+		notify  => Service["zarafa-server"]
+	}
+}
+
+class zarafa::webapp inherits php::apache2 {
+	include zarafa::apt
+
+	package {"zarafa-webaccess":
+		ensure  => installed,
+		require => Apt::Deb-list["canonical-lucid-partner"]
 	}
 }

9d7da6cf69300fee0df1106e5e99b24b615de739
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-05-12 14:04:07 +0200

    New classes development::{debian,ubuntu}.

--- development/manifests/init.pp
+++ development/manifests/init.pp
@@ -4,3 +4,11 @@ class development::utils {
 	package {"bzr": ensure => installed}
 	package {"git-core": ensure => installed}
 }
+
+class development::debian {
+	package {["dput", "devscripts", "fakeroot", "build-essential"]: ensure => installed}
+}
+
+class development::ubuntu inherits development::debian {
+	package {["wget", "ubuntu-dev-tools"]: ensure => installed}
+}

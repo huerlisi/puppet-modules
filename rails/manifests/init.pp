979b1daef2339af0fa0fd1d966f9fda6fb704e3b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-07-12 09:06:33 +0200

    New classes rails3::{framework,webapp}.

--- rails/manifests/init.pp
+++ rails/manifests/init.pp
@@ -17,3 +17,21 @@ class rails::webapp {
 	include apache2::server
 	include passenger::apache2
 }
+
+# Rails 3
+class rails3::framework {
+	package {"rubygems": ensure => present}
+
+	package {"rails":
+		provider => 'gem',
+		ensure   => '3.0.0.beta4',
+		require  => Package["rubygems"]
+	}
+}
+
+class rails3::webapp {
+	include rails3::framework
+	include apache2::server
+	include passenger::apache2
+}
+

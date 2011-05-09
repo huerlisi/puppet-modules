1ab3297019e4fd6bb9408827ab4708ab41d26ca3
David Hürlimann <david.huerlimann@cyt.ch>
2011-05-09 14:12:52 +0200

    Added new concept of using nagios files. So i can easy raise the critical and warning states.

--- nagios/manifests/init.pp
+++ nagios/manifests/init.pp
@@ -1,8 +1,34 @@
 # Nagios
 # ======
+# 
+# Installs the package nagios-nrpe-server. This starts all needed daemons and services for nagios3-server.
+# Starts the service nagios-nrpe-server.
 #
-# Installs the package nagios3.
-#
-class nagios::server {
-	package {"nagios3": ensure => installed}
+class nagios::cyt-client {
+        package {"nagios-nrpe-server": ensure => installed }
+        service {"nagios-nrpe-server":
+                ensure  => running,
+		pattern => 'nrpe',
+                require => Package["nagios-nrpe-server"]
+	}
+        file { "/etc/nagios/nrpe.cfg":
+                content => template('nagios/etc/nagios/nrpe.cfg'),
+                notify  => Service["nagios-nrpe-server"],
+		require => Package["nagios-nrpe-server", "nagios-plugins-standard"]
+        }
+
+        package {"nagios-plugins-standard": ensure => installed }
+	package {"snmpd": ensure => installed }
+	service {"snmpd":
+		ensure 	=> running,
+		pattern	=> 'snmpd',
+		require => Package["snmpd"]
+	}
+        file { "/etc/snmp/snmpd.conf":
+                content => template('nagios/etc/snmp/snmpd.conf'),
+                notify  => Service["snmpd"],
+		require => Package["snmpd", "nagios-snmp-plugins"]
+        }
+
+	package {"nagios-snmp-plugins": ensure => installed }
 }

35c32364825eafdf93f926e4fc76c8986b488f79
David Hürlimann <david.huerlimann@cyt.ch>
2011-11-25 01:13:19 +0100

    Added new puppet init.pp file for setup bacula.

---
+++ bacula/manifests/init.pp
@@ -0,0 +1,20 @@
+# Bacula
+# ======
+# 
+# Installs the package nagios-nrpe-server. This starts all needed daemons and services for nagios3-server.
+# Starts the service nagios-nrpe-server.
+#
+class bacula::cyt-client {
+        package {"bacula-client": ensure => installed }
+        service {"bacula-fd":
+                ensure  => running,
+		pattern => 'bacula-fd',
+                require => Package["bacula-fd"]
+	}
+        file { "/etc/bacula/bacula-fd.conf":
+                content => template('bacula/etc/bacula/bacula-fd.conf'),
+                notify  => Service["bacula-fd"],
+		require => Package["bacula-fd"]
+        }
+
+}

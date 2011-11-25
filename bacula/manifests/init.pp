69b80a7018fb9f1eca4b5fc59539331a2621c67e
David Hürlimann <david.huerlimann@cyt.ch>
2011-11-25 01:24:56 +0100

    Changed bacula manifest file to work correctly.

--- bacula/manifests/init.pp
+++ bacula/manifests/init.pp
@@ -4,17 +4,17 @@
 # Installs the package nagios-nrpe-server. This starts all needed daemons and services for nagios3-server.
 # Starts the service nagios-nrpe-server.
 #
-class bacula::cyt-client {
+class bacula::client {
         package {"bacula-client": ensure => installed }
         service {"bacula-fd":
                 ensure  => running,
 		pattern => 'bacula-fd',
-                require => Package["bacula-fd"]
+                require => Package["bacula-client"]
 	}
         file { "/etc/bacula/bacula-fd.conf":
                 content => template('bacula/etc/bacula/bacula-fd.conf'),
                 notify  => Service["bacula-fd"],
-		require => Package["bacula-fd"]
+		require => Package["bacula-client"]
         }
 
 }

160fc20f36509cd1e81439ff8f00917213bd993f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2014-05-14 07:49:50 +0200

    Whitespace fixes for nagios manifest.

--- nagios/manifests/init.pp
+++ nagios/manifests/init.pp
@@ -5,30 +5,30 @@
 # Starts the service nagios-nrpe-server.
 #
 class nagios::cyt-client {
-        package {"nagios-nrpe-server": ensure => installed }
-        service {"nagios-nrpe-server":
-                ensure  => running,
-		pattern => 'nrpe',
-                require => Package["nagios-nrpe-server"]
-	}
-        file { "/etc/nagios/nrpe.cfg":
-                content => template('nagios/etc/nagios/nrpe.cfg'),
-                notify  => Service["nagios-nrpe-server"],
-		require => Package["nagios-nrpe-server", "nagios-plugins-standard"]
-        }
+  package {"nagios-nrpe-server": ensure => installed }
+  service {"nagios-nrpe-server":
+    ensure  => running,
+    pattern => 'nrpe',
+    require => Package["nagios-nrpe-server"]
+  }
+  file { "/etc/nagios/nrpe.cfg":
+    content => template('nagios/etc/nagios/nrpe.cfg'),
+    notify  => Service["nagios-nrpe-server"],
+    require => Package["nagios-nrpe-server", "nagios-plugins-standard"]
+  }
 
-        package {"nagios-plugins-standard": ensure => installed }
-	package {"snmpd": ensure => installed }
-	service {"snmpd":
-		ensure 	=> running,
-		pattern	=> 'snmpd',
-		require => Package["snmpd"]
-	}
-        file { "/etc/snmp/snmpd.conf":
-                content => template('nagios/etc/snmp/snmpd.conf'),
-                notify  => Service["snmpd"],
-		require => Package["snmpd", "nagios-snmp-plugins"]
-        }
+  package {"nagios-plugins-standard": ensure => installed }
+  package {"snmpd": ensure => installed }
+  service {"snmpd":
+    ensure 	=> running,
+    pattern	=> 'snmpd',
+    require => Package["snmpd"]
+  }
+  file { "/etc/snmp/snmpd.conf":
+    content => template('nagios/etc/snmp/snmpd.conf'),
+    notify  => Service["snmpd"],
+    require => Package["snmpd", "nagios-snmp-plugins"]
+  }
 
-	package {"nagios-snmp-plugins": ensure => installed }
+  package {"nagios-snmp-plugins": ensure => installed }
 }

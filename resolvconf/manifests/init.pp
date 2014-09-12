a2a5e66b98b2350a9c88bdc388ab828db584cb07
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2014-09-12 08:38:09 +0200

    Ensure resolv.conf original template is empty.

--- resolvconf/manifests/init.pp
+++ resolvconf/manifests/init.pp
@@ -4,5 +4,15 @@
 # Installs the package resolvconf.
 #
 class resolvconf::client {
-        package {"resolvconf": ensure => installed}
+  package {"resolvconf": ensure => installed}
+  service {"resolvconf":
+    ensure  => running,
+    require => Package['resolvconf']
+  }
+
+  file {"/etc/resolvconf/resolv.conf.d/original":
+    ensure  => present,
+    content => '',
+    notify  => Service['resolvconf']
+  }
 }
